c53bb81e1929b6da118f2bd35b01bed4bd7a0200
Mauricio Junior <mauricio.junior@gmail.com>
2023-11-21 21:56:06 -0600

    add project
    add project

---
+++ VerificaCPFeCNPJ/VerificaCPFeCNPJ/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var checkCPFCNPJ = new CPFCNPJ.Main();
+
+var result = checkCPFCNPJ.IsValidCPFCNPJ("90099299033");
+
+Console.WriteLine("Result:" + result);
